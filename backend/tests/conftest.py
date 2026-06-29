39f7ba38ebce0be18f943428009a7baba6e694d2
Youssef <youssef.itani.11.11@gmail.com>
2026-06-29 18:36:25 +0300

    Implemented JWT Utility Functions tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,3 +2,4 @@ import os
 from cryptography.fernet import Fernet
 
 os.environ.setdefault("ENCRYPTION_KEY", Fernet.generate_key().decode())
+os.environ.setdefault("JWT_SECRET", Fernet.generate_key().decode())
