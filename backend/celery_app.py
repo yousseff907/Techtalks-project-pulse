0cfe16ae864f6fb8bc11e7f8f8202eed13e207a3
Youssef <youssef.itani.11.11@gmail.com>
2026-07-08 00:37:19 +0300

    added celery and beat to dockercompose

---
+++ backend/celery_app.py
@@ -0,0 +1,10 @@
+from celery import Celery
+from config import REDIS_URL
+from celery.schedules import crontab
+
+celery_app = Celery("project-pulse", broker=REDIS_URL, backend=REDIS_URL)
+
+celery_app.conf.beat_schedule = {"sync across all workspaces" : {"task" : "celery_app.sync_all_active_workspaces",
+																 "schedule" : crontab(minute="*/30")
+																}
+								}
